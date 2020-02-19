dd2aadc11a46859912bd57775865a313c8b826ad
Sauraen <sauraen@gmail.com>
2020-02-18 22:26:17 -0800

    Working on bootstrap

--- python/ootbootstrap.py
+++ python/ootbootstrap.py
@@ -9,29 +9,34 @@ def jumpcmd(addr):
     j = ((addr >> 2) & 0x03FFFFFF) | 0x08000000
     return j.to_bytes(4, byteorder='big')
 
-def bootstrapper1(data, s1startoffset, jrraaddr):
+def bootstrapper1and3(bsidx, data, regstartoffset, jrraaddr):
+    #TODO optimize this so it writes the same literal to multiple addresses if
+    #they're the same. There will be many of these.
     assert(len(data) % 2 == 0)
-    if(s1startoffset < 0):
-        s1startoffset += 0x10000
-    c3data = jumpcmd(jrraaddr)
+    assert(bsidx == 1 or bsidx == 3)
+    if(regstartoffset < 0):
+        regstartoffset += 0x10000
     ret = bytearray()
+    def write_frame(c1data):
+        frame = c1data + bytes([0]*4) + jumpcmd(jrraaddr) + bytes([0]*4)
+        ret.extend(frame if bsidx == 3 else frame * 3)
     d = 0
     while d < len(data):
         #addi $gp, $r0, 0xIIII
         #00100000 00011100 IIIIIIII IIIIIIII
-        ret.extend(b'\x20\x1C' + bytes([data[d], data[d+1]])) 
-        ret.extend(bytes([0]*4) + c3data + bytes([0]*4))
-        #sh $gp, 0xKKKK($s1)
-        #10100110 00111100 KKKKKKKK KKKKKKKK
-        ret.extend(b'\xA6\x3C' + s1startoffset.to_bytes(2, byteorder='big'))
-        ret.extend(bytes([0]*4) + c3data + bytes([0]*4))
+        write_frame(b'\x20\x1C' + bytes([data[d], data[d+1]]))
+        #sh $gp, 0xKKKK($s1 for bs1, $s0 for bs3)
+        #10100110 00111100 KKKKKKKK KKKKKKKK for bs1
+        #10100110 00011100 KKKKKKKK KKKKKKKK for bs3
+        byte2 = b'\x1C' if bsidx == 3 else b'\x3C'
+        write_frame(b'\xA6' + byte2 + regstartoffset.to_bytes(2, byteorder='big'))
         d += 2
-        s1startoffset += 2
+        regstartoffset += 2
+    print('Bootstrapper' + str(bsidx) + ' is ' + str(float(len(ret)//16)/60) + ' sec.')
     return ret
 
 def jumpsingle(addr):
-    zeros = bytes([0, 0, 0, 0])
-    return zeros + zeros + jumpcmd(addr) + zeros
+    return bytes([0]*8) + jumpcmd(addr) + bytes([0]*4)
     
 def dataforbootstrapper4(data, bs4addr):
     assert(len(data) % 8 == 0)
@@ -41,9 +46,11 @@ def dataforbootstrapper4(data, bs4addr):
     while d < len(data):
         c3data = bytes([data[d  ], data[d+1] & 0x3F, data[d+2], data[d+3]])
         c4data = bytes([data[d+4], data[d+5] & 0x3F, data[d+6], data[d+7]])
-        c2data = bytes([0, 0, (data[d+5] & 0xC0) >> 6, data[d+1] & 0xC0])
+        c2b4 = ((data[d+5] & 0xC0) >> 4) | ((data[d+1] & 0xC0) >> 6)
+        c2data = bytes([0, 0, 0, c2b4])
         ret.extend(c1data + c2data + c3data + c4data)
         d += 8
+    print('Kargaroc payload injection is ' + str(float(len(ret)//16)/60) + ' sec.')
     return ret
 
 def walk_into_bs1(jrraaddr):
@@ -53,21 +60,23 @@ def walk_into_bs1(jrraaddr):
 
 def ootbootstraprun(bs2data, bs4data, maindata):
     jrraaddr = 0x80000490
-    s1 = 0x801C84A0 #global context
-    bs24loc = 0x801C7E70 #must be within 0x8000 of global context
+    bs1s1 = 0x801C84A0 #global context
+    bs2loc = 0x801C7E70 #must be within 0x8000 of global context
+    bs3s0 = 0x8011D500 #padmgr
+    bs4loc = 0x8011E000 #must be within 0x8000 of padmgr
     kargaroc_loader_entry = 0x80401000
     ret = bytearray()
-    ret.extend(walk_into_bs1(jrraaddr) * 20) #frames of walking
-    ret.extend(bootstrapper1(bs2data, bs24loc - s1, jrraaddr))
-    ret.extend(jumpsingle(bs24loc))
-    ret.extend(bootstrapper1(bs4data, bs24loc - s1, jrraaddr))
-    ret.extend(dataforbootstrapper4(maindata, bs24loc))
+    ret.extend(walk_into_bs1(jrraaddr) * 60) #frames of walking
+    ret.extend(bootstrapper1and3(1, bs2data, bs2loc - bs1s1, jrraaddr))
+    ret.extend(jumpsingle(bs2loc) * 3)
+    ret.extend(bootstrapper1and3(3, bs4data, bs4loc - bs3s0, jrraaddr))
+    ret.extend(dataforbootstrapper4(maindata, bs4loc))
     ret.extend(jumpsingle(kargaroc_loader_entry) * 100) #frames of running K's loader
     return m64.create_header(4, len(ret) // 16) + ret
     
 if __name__ == '__main__':
     try:
-        bs2data = open('bootstrapper2.bin', 'rb').read()
+        bs2data = open('newbs2.bin', 'rb').read()
         bs4data = open('bootstrapper4.bin', 'rb').read()
         maindata = open('kargaroc_loader.bin', 'rb').read()
         out = open(sys.argv[1], 'wb')
